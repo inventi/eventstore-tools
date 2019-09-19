6bafc7d159d7f0aeb4a2e56ebe0d23cf924c5cd6
Karolis Labrencis <karolis@labrencis.lt>
2019-09-19 17:32:31 +0300

    fix: Throw runtimeExceptions to avoid UndeclaredException in  TransactionTemplate

--- src/main/kotlin/io/inventi/eventstore/eventhandler/IdempotentEventHandler.kt
+++ src/main/kotlin/io/inventi/eventstore/eventhandler/IdempotentEventHandler.kt
@@ -75,7 +75,7 @@ abstract class IdempotentEventHandler(
         val persistentSubscription = object : PersistentSubscriptionListener {
 
             override fun onClose(subscription: PersistentSubscription?, reason: SubscriptionDropReason, exception: Exception?) {
-                logger.warn("Subscription StreamName: $streamName GroupName: $groupName was closed. Reason: $reason")
+                logger.warn("Subscription StreamName: $streamName GroupName: $groupName was closed. Reason: $reason", exception)
                 when {
                     exception is EventStoreException -> {
                         logger.warn("Eventstore connection lost: ${exception.message}")
@@ -87,7 +87,7 @@ abstract class IdempotentEventHandler(
                         eventStore.subscribeToPersistent(streamName, groupName, this)
                     }
                     exception != null -> {
-                        throw exception
+                        throw RuntimeException(exception)
                     }
                 }
             }
@@ -117,7 +117,7 @@ abstract class IdempotentEventHandler(
                             subscription.acknowledge(eventMessage)
                             return@execute
                         }
-                        throw e
+                        throw RuntimeException(e)
                     }
 
                     this@IdempotentEventHandler::class.java
@@ -149,8 +149,9 @@ abstract class IdempotentEventHandler(
                             "eventType: ${event.eventType}, " +
                             "streamName: $streamName, " +
                             "groupName: $groupName, " +
-                            "eventData: \n${String(event.data)}")
-                    throw e
+                            "eventData: \n${String(event.data)}",
+                            e)
+                    throw RuntimeException(e)
                 }
             }
 
