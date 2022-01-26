8d04d4304f7d2045a39d0344f4b8da84639a5aae
Žilvinas Bliudžius <zilvinas.bliudzius@inventi.lt>
2022-01-26 14:36:27 +0200

    fix(BO-1808): do not depend on configuration order when creating tracing extension

--- src/main/kotlin/io/inventi/eventstore/eventhandler/tracing/TracingExtension.kt
+++ src/main/kotlin/io/inventi/eventstore/eventhandler/tracing/TracingExtension.kt
@@ -7,13 +7,11 @@ import com.github.msemys.esjc.RecordedEvent
 import io.inventi.eventstore.eventhandler.Cleanup
 import io.inventi.eventstore.eventhandler.EventHandlerExtension
 import io.inventi.eventstore.eventtracing.EventMetadataTraceExtractor
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Component
 import java.lang.reflect.Method
 
 @Component
-@ConditionalOnBean(Tracing::class)
 @ConditionalOnProperty("eventstore.tracing.enabled", matchIfMissing = true)
 class TracingExtension(
         private val tracing: Tracing,
