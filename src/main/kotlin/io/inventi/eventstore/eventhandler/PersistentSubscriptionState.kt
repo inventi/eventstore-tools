a00967ed8acc4f66fbc1d2907af6957da9b1ecea
Žilvinas Bliudžius <zilvinas.bliudzius@inventi.lt>
2022-08-05 10:36:59 +0300

    fix(BOS-310): ignore subscription stoppage timeouts

--- src/main/kotlin/io/inventi/eventstore/eventhandler/PersistentSubscriptionState.kt
+++ src/main/kotlin/io/inventi/eventstore/eventhandler/PersistentSubscriptionState.kt
@@ -1,9 +1,18 @@
 package io.inventi.eventstore.eventhandler
 
 import com.github.msemys.esjc.PersistentSubscription
+import io.inventi.eventstore.util.LoggerDelegate
+import org.slf4j.Logger
 import java.time.Duration
+import java.util.concurrent.TimeoutException
 
-data class PersistentSubscriptionState(internal var subscription: PersistentSubscription? = null) {
+data class PersistentSubscriptionState(private var subscription: PersistentSubscription? = null) {
+    companion object {
+        private val logger: Logger by LoggerDelegate()
+
+        private val STOPPAGE_TIMEOUT = Duration.ofSeconds(60)
+    }
+
     internal val isActive get() = subscription != null
 
     fun update(newSubscription: PersistentSubscription) {
@@ -14,7 +23,11 @@ data class PersistentSubscriptionState(internal var subscription: PersistentSubs
     fun drop() {
         subscription?.let {
             subscription = null
-            it.stop(Duration.ofSeconds(60))
+            try {
+                it.stop(STOPPAGE_TIMEOUT)
+            } catch (e: TimeoutException) {
+                logger.warn("Persistent subscription $subscription did not stop in time ($STOPPAGE_TIMEOUT)")
+            }
         }
     }
 }
